f994addadb182cf2ade6da2b819f14a553763107
anatonokao <anato.devs@gmail.com>
2024-01-28 15:07:35 +0300

    feat: implement parse env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,30 @@
+import process from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_foo = "bar";
+  process.env.RSS_bar = 1234;
+
+  const rssFields = getEnvByPrefix("RSS_");
+  if (rssFields.length === 0) {
+    const msg = "No RSS_* environment variables found";
+    console.log(colorizeText(msg, "\x1b[33m"));
+  } else {
+    rssFields.forEach(([key, value]) => {
+      console.log(
+        `${colorizeText(key, "\x1b[34m")} = ${colorizeText(value, "\x1b[32m")};`
+      );
+    });
+  }
 };
+
+function getEnvByPrefix(prefix) {
+  return Object.entries(process.env).filter(([key, _]) =>
+    key.startsWith(prefix)
+  );
+}
+
+function colorizeText(text, color) {
+  return color + text + "\x1b[0m";
+}
 
 parseEnv();
