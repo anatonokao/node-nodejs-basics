8d954afe0681773a416e36cef0fea1f8ee2de0f9
anatonokao <anato.devs@gmail.com>
2024-01-29 07:18:24 +0300

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import process from "node:process";
+
 const transform = async () => {
-    // Write your code here 
+  process.stdout.write(colorizeText("Write something: ", "\x1b[33m"));
+  process.openStdin().on("data", (chunk) => {
+    const reversed = chunk.toString().split("").reverse().join("");
+
+    process.stdout.write(
+      colorizeText(`Reversed: ${reversed.trim()}`, "\x1b[32m")
+    );
+    process.exit(0);
+  });
 };
+
+function colorizeText(text, color) {
+  return color + text + "\x1b[0m";
+}
 
 await transform();
