f3ea0afd7ec81613f945d00df1273104b2d83fa4
anatonokao <anato.devs@gmail.com>
2024-01-29 07:02:03 +0300

    refactor: replace console log with stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,18 +1,24 @@
 import fs from "node:fs";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
+import process from "node:process";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const FILE_PATH = `${__dirname}/files/fileToRead.txt`;
 
 const read = async () => {
   const stream = fs.createReadStream(FILE_PATH);
+
   stream.on("data", (data) => {
-    console.log(colorizeText("data: ", "\x1b[33m"), data.toString());
+    process.stdout.write(
+      colorizeText("data: ", "\x1b[33m") + data.toString() + "\n"
+    );
   });
 
   stream.on("end", () => {
-    console.log(colorizeText("File readed successfully", "\x1b[32m"));
+    process.stdout.write(
+      colorizeText("File readed successfully", "\x1b[32m") + "\n"
+    );
   });
 };
 
