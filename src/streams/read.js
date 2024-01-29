90e38e4cfc6e4c45584f29f9310ff69ec89aa906
anatonokao <anato.devs@gmail.com>
2024-01-29 06:59:11 +0300

    feat: implement read function with stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import fs from "node:fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const FILE_PATH = `${__dirname}/files/fileToRead.txt`;
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(FILE_PATH);
+  stream.on("data", (data) => {
+    console.log(colorizeText("data: ", "\x1b[33m"), data.toString());
+  });
+
+  stream.on("end", () => {
+    console.log(colorizeText("File readed successfully", "\x1b[32m"));
+  });
 };
+
+function colorizeText(text, color) {
+  return color + text + "\x1b[0m";
+}
 
 await read();
