5b130ae2ee8763b76792ade7010da6c6ddfb00ce
anatonokao <anato.devs@gmail.com>
2024-01-29 20:46:22 +0300

    feat: rework worker, implement perform calculation function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import { Worker } from "node:worker_threads";
+import os from "node:os";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const WORKER_PATH = `${__dirname}/worker.js`;
+const cpuCores = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const res = [];
+
+  const workers = createWorkers(cpuCores, WORKER_PATH);
+
+  workers.forEach((worker) => {
+    worker.on("message", (data) => {
+      res.splice(data.index, 0, {
+        status: "resolved",
+        data: data.res,
+      });
+      if (res.length === cpuCores) {
+        console.log(res);
+        process.exit(0);
+      }
+    });
+
+    worker.on("error", (data) => {
+      res.splice(data.index, 0, {
+        status: "error",
+        data: null,
+      });
+      if (res.length === cpuCores) {
+        console.log(res);
+        process.exit(0);
+      }
+    });
+  });
+
+  workers.forEach((worker, index) => {
+    worker.postMessage({ n: 10 + index + 1, index });
+  });
 };
+
+function createWorkers(n, worker) {
+  return Array.from({ length: cpuCores }, () => new Worker(WORKER_PATH));
+}
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,29 +1,16 @@
-import process from "node:process";
+import { parentPort } from "node:worker_threads";
 
 // n should be received from main thread
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  process.stdout.write(colorizeText("Write something: ", "\x1b[33m"));
-  process.openStdin().on("data", (chunk) => {
-    const number = parseInt(chunk.toString());
-    if (isNaN(number)) {
-      process.stdout.write(
-        colorizeText("Invalid input. Write a number.\n", "\x1b[31m")
-      );
-      process.exit(0);
-    }
-
-    process.stdout.write(
-      colorizeText(`Result: ${nthFibonacci(number)}`, "\x1b[32m")
-    );
-    process.exit(0);
+  parentPort.on("message", (workerData) => {
+    parentPort.postMessage({
+      res: nthFibonacci(workerData.n),
+      index: workerData.index,
+    });
   });
 };
-
-function colorizeText(text, color) {
-  return color + text + "\x1b[0m";
-}
 
 sendResult();
