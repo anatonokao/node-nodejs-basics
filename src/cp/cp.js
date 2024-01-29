ca6b53cd44dcef8f1edb23f668e9153d3f5945a6
anatonokao <anato.devs@gmail.com>
2024-01-29 21:19:21 +0300

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "node:child_process";
+
+const spawnChildProcess = async (...args) => {
+  const child = spawn("node", ["./cp/files/script.js", ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(1, 2, 3);
